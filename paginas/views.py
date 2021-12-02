4229532d6cb1955c64f43ac67af849ce5993e3e1
gasp-lara <laragasparottooroque@gmail.com>
2021-12-02 18:46:24 -0300

    ^modelo, sobre e bootstrap

--- paginas/views.py
+++ paginas/views.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 class PaginaInicial(TemplateView):
     template_name = 'paginas/index.html'
 
-class PaginaSobre(TemplateView):
+class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'
 
