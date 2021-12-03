6ed8b2997ed3b11f252d86a7d84cdf946b217365
gasp-lara <laragasparottooroque@gmail.com>
2021-12-03 11:27:35 -0300

    create e update

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,50 +1,6 @@
 from django.db import models
-from django.db.models.fields import BooleanField, FloatField, IntegerField
-from django.db.models.fields.files import ImageField
 
 # Create your models here.
-class Cliente(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.CharField(max_length=60)
-    senha = models.CharField(max_length=10)
-    cep = models.CharField(max_length=8)
-    endereco = models.CharField(max_length=20)
-    numero = models.CharField(max_length=5)
-    bairro = models.CharField(max_length=20)
-    cidade = models.CharField(max_length=20)
-    estado = models.CharField(max_length=20)
-    dt_nasc = models.DateField
-    dt_cadastro = models.DateField
-
-    def _str_(self):
-        return self.nome + ' - ' + str(self.email) + ' - ' + str(self.senha) + ' - ' + str(self.cep) + ' - ' + str(self.endereco) + ' - ' + str(self.numero) + ' - ' + str(self.bairro) + ' - ' + str(self.cidade) + ' - ' + str(self.estado) + ' - ' +  str(self.dt_nasc) + ' - ' +   str(self.dt_cadastro)
-
-
-class Livro(models.Model):
-# usuario = Cliente
-    titulo = models.CharField(max_length=50)
-    autor = models.CharField(max_length=50)
-    editora = models.CharField(max_length=50)
-    descricao = models.CharField(max_length=50)
-    dt_cadastroLivro = models.DateField
-    foto = models.ImageField
-    ano_pub = models.IntegerField
-    adicionar = models.CharField(max_length=50)
-    vendido = models.BooleanField
-    valor = models.FloatField
-# categoria = Categoria 
-
-    def _str_(self):
-        return self.titulo + ' - ' + str(self.autor) + ' - ' + str(self.editora) + ' - ' + str(self.descricao) + ' - ' + str(self.dt_cadastro) + ' - ' + str(self.foto) + ' - ' + str(self.ano_pub) + ' - ' + str(self.adicionar) + ' - ' + str(self.vendido) + ' - ' +  str(self.valor) + ' - ' +   str(self.categoria)
-
-class Venda (models.Model):
-    data_venda = models.DateField
-    cliente = models.CharField(max_length=50)
-    valor = FloatField
-
-    def _str_(self): 
-        return self.data_venda + ' - ' + str(self.cliente) + ' - ' + str(self.valor)
-
 
 class Categoria (models.Model):
     nome = models.CharField(max_length=50)
@@ -57,6 +13,51 @@ class FormaDePagamento (models.Model):
 
      def _str_(self): 
         return self.descricao
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.CharField(max_length=60)
+    senha = models.CharField(max_length=10)
+    cep = models.CharField(max_length=8)
+    endereco = models.CharField(max_length=20)
+    numero = models.CharField(max_length=5)
+    bairro = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=20)
+    estado = models.CharField(max_length=20)
+    dt_nasc = models.DateField()
+    dt_cadastro = models.DateField()
+
+    def _str_(self):
+        return self.nome + ' - ' + str(self.email)
+
+
+class Livro(models.Model):
+# usuario = Cliente
+    titulo = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50)
+    editora = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=50)
+    dt_cadastroLivro = models.DateField()
+    foto = models.ImageField()
+    ano_pub = models.IntegerField()
+    adicionar = models.CharField(max_length=50)
+    vendido = models.BooleanField()
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+
+    def _str_(self):
+        return self.titulo + ' - ' + str(self.autor)
+
+class Venda (models.Model):
+    data_venda = models.DateField()
+    cliente = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def _str_(self): 
+        return self.data_venda + ' - ' + str(self.cliente) + ' - ' + str(self.valor)
+
 
 
 
--- cadastros/views.py
+++ cadastros/views.py
@@ -8,33 +8,72 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 class ClienteCreate(CreateView):
     model = Cliente
     fields = ["nome", "email", "senha", "cep", "endereco", "numero", "bairro", "cidade", "estado", "dt_nasc", "dt_cadastro"]
-    template_name = "cadastros/cadastros.html"
+    template_name = "cadastros/form.html"
     success_url = reverse_lazy("")
 
     
 class LivroCreate(CreateView):
     model = Livro
     fields = ["usuario", "titulo", "autor", "editora", "descricao", "dt_cadastro", "foto", "ano_pub", "adicionar", "vendido", "valor", "categoria"]
-    template_name = "cadastros/cadastros.html"
+    template_name = "cadastros/form.html"
     success_url = reverse_lazy("")
 
 
 class VendaCreate(CreateView):
     model = Venda
     fields = ["dt_venda", "cliente", "valor"]
-    template_name = "cadastros/cadastros.html"
+    template_name = "cadastros/form.html"
     success_url = reverse_lazy("")
 
 
 class CategoriaCreate(CreateView):
     model = Categoria
     fields = ["nome"]
-    template_name = "cadastros/cadastros.html"
+    template_name = "cadastros/form.html"
     success_url = reverse_lazy("")
 
 
 class FormaDePagamentoCreate(CreateView):
     model = FormaDePagamento
     fields = ["nome"]
-    template_name = "cadastros/cadastros.html"
+    template_name = "cadastros/form.html"
     success_url = reverse_lazy("")
+
+    ##update
+
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    fields = ["nome", "email", "senha", "cep", "endereco", "numero", "bairro", "cidade", "estado", "dt_nasc", "dt_cadastro"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("")
+
+    
+class LivroUpdate(UpdateView):
+    model = Livro
+    fields = ["usuario", "titulo", "autor", "editora", "descricao", "dt_cadastro", "foto", "ano_pub", "adicionar", "vendido", "valor", "categoria"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("")
+
+
+class VendaUpdate(UpdateView):
+    model = Venda
+    fields = ["dt_venda", "cliente", "valor"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("")
+
+
+class CategoriaUpdate(CUpdateView):
+    model = Categoria
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("")
+
+
+class FormaDePagamentoUpdate(UpdateView):
+    model = FormaDePagamento
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("")
+
+##Delete
+
