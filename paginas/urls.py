e40156f92f26893513934eef2b264f5050e0aafc
gasp-lara <laragasparottooroque@gmail.com>
2021-12-02 22:19:36 -0300

    Funcionando no navegador

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,11 +1,11 @@
 from os import name
 from django.urls import path
-from .views import PaginaInicial
-
+from .views import PaginaInicial, SobreView
 
 urlpatterns = [
     # Criar todos os endereços/rotas
     # path('endereço/', MinhaView.as_view(), name='referência/apelido'),
-    path('inicio/', PaginaInicial.as_view(), name='index'),
+    path('', PaginaInicial.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
     
 ]
